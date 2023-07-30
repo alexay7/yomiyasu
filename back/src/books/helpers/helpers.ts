9380abccdafdf3f37eb24ea1ced61ed623e63ad5
alexay7 <43906716+alexay7@users.noreply.github.com>
2023-07-30 19:03:46 +0200

    Fixed character count

--- back/src/books/helpers/helpers.ts
+++ back/src/books/helpers/helpers.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from "fs";
 import {dirname, join} from "path";
+import {load} from "cheerio";
 
 async function extractFirstFileNameFromFolder(folderPath: string): Promise<string | null> {
     const files = await fs.readdir(folderPath);
@@ -21,24 +22,29 @@ export async function getCharacterCount(bookPath:string):Promise<number> {
     const htmlContent = await fs.readFile(bookPath, "utf8");
 
     const japaneseRegex = /[\u3040-\u30FF\u4E00-\u9FFF]/g; // Expresión regular para kanji (U+4E00 - U+9FFF) y kana (U+3040 - U+30FF)
-    let totalJapaneseCharacters = 0;
+    const $ = load(htmlContent);
+
+    let japaneseCharacterCount = 0;
 
     // Buscar todos los elementos <div> con clase "textBox"
-    const textBoxDivs = htmlContent.match(/<div[^>]*class="textBox"[^>]*>[\s\S]*?<\/div>/g) || [];
-
-    textBoxDivs.forEach((div) => {
-    // Dentro de cada div.textBox, buscar todos los elementos <p>
-        const pElements = div.match(/<p[^>]*>[\s\S]*?<\/p>/g) || [];
-
-        pElements.forEach((pElement) => {
-            // Filtrar y contar caracteres japoneses en el texto de cada elemento <p>
-            const text = pElement.replace(/<[^>]+>/g, ""); // Eliminar las etiquetas HTML del texto
-            const japaneseCharacters = text.match(japaneseRegex) || [];
-            totalJapaneseCharacters += japaneseCharacters.length;
+    $("div.textBox").each((index, element) => {
+        // Dentro de cada div.textBox, buscar todos los elementos <p>
+        $(element).find("p").each((i, pElement) => {
+        // Obtener el texto de cada elemento <p>
+            const text = $(pElement).text();
+  
+            // Usar expresión regular para filtrar solo los caracteres japoneses
+            const japaneseCharacters = text.match(japaneseRegex);
+  
+            // Sumar la cantidad de caracteres japoneses encontrados
+            if (japaneseCharacters) {
+                japaneseCharacterCount += japaneseCharacters.length;
+            }
         });
     });
 
-    return totalJapaneseCharacters;
+
+    return japaneseCharacterCount * 0.9;
 }
 
 export async function extractUrlFromHtml(
