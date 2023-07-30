c8511e7ff20b47ce2c04d5ec5312732ae32d8693
alexay7 <43906716+alexay7@users.noreply.github.com>
2023-07-30 13:36:20 +0200

    Fixed progress not registering

--- front/src/helpers/progress.ts
+++ front/src/helpers/progress.ts
@@ -2,18 +2,23 @@ import {api} from "../api/api";
 import {Book, BookProgress} from "../types/book";
 
 export async function createProgress(bookData:Book, page:number, time:number):Promise<void> {
+    let currentPage = page;
+    if (currentPage > bookData.pages) {
+        currentPage = bookData.pages;
+    }
+
     const newProgress:BookProgress = {
         book:bookData._id,
         time,
-        currentPage:page,
+        currentPage:currentPage,
         status:"unread"
     };
 
-    if (bookData.pages === page) {
+    if (bookData.pages <= currentPage) {
         // Libro terminado
         newProgress.status = "completed";
         newProgress.endDate = new Date();
-    } else if (page > 0) {
+    } else if (currentPage > 0) {
         // progreso normal
         newProgress.status = "reading";
     }
